16544f4d61ad110453ecf6d800bf809222bc533b
G285112022 <228511@st.takushoku-u.ac.jp>
2024-11-27 17:35:37 +0900

    自由課題の途中経過
    天体の作成

--- ap0901.js
+++ ap0901.js
@@ -1,6 +1,6 @@
 //
 // 応用プログラミング 第9,10回 自由課題 (ap0901.js)
-// G38400-2023 拓殖太郎
+// G285112022 三井星夜
 //
 "use strict"; // 厳格モード
 
@@ -28,14 +28,51 @@ function init() {
 
   // カメラの作成
   const camera = new THREE.PerspectiveCamera(
-    50, window.innerWidth/window.innerHeight, 0.1, 1000);
-  camera.position.set(1,2,3);
-  camera.lookAt(0,0,0);
+    50, window.innerWidth / window.innerHeight, 0.1, 1000);
+  camera.position.set(10, 20, 30);
+  camera.lookAt(0, 0, 0);
 
   // レンダラの設定
   const renderer = new THREE.WebGLRenderer();
   renderer.setSize(window.innerWidth, innerHeight);
-    document.getElementById("output").appendChild(renderer.domElement);
+  document.getElementById("output").appendChild(renderer.domElement);
+
+  //　環境光
+  const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+  scene.add(ambientLight);
+  // 太陽光
+  const pointLight = new THREE.PointLight(0xffffff, 1);
+  pointLight.position.set(0, 0, 0);
+  scene.add(pointLight);
+
+  // 太陽
+  const sunGeometry = new THREE.SphereGeometry(5, 32, 32);
+  const sunMaterial = new THREE.MeshBasicMaterial({ color: 0xffcc00 }); 
+  const sun = new THREE.Mesh(sunGeometry, sunMaterial);
+  scene.add(sun);
+
+  // 地球
+  const earthGeometry = new THREE.SphereGeometry(2, 32, 32);
+  const earthMaterial = new THREE.MeshStandardMaterial({ color: 0x0000ff });
+  const earth = new THREE.Mesh(earthGeometry, earthMaterial);
+
+  // 地球の軌道グループ
+  const earthOrbit = new THREE.Group();
+  scene.add(earthOrbit);
+  earth.position.set(10, 0, 0);
+  earthOrbit.add(earth);
+
+  // 月
+  const moonGeometry = new THREE.SphereGeometry(0.5, 32, 32);
+  const moonMaterial = new THREE.MeshStandardMaterial({ color: 0x888888 });
+  const moon = new THREE.Mesh(moonGeometry, moonMaterial);
+
+  // 月の軌道グループ
+  const moonOrbit = new THREE.Group();
+  earthOrbit.add(moonOrbit);
+  moon.position.set(3, 0, 0);
+  moonOrbit.add(moon);
+
 
   // 描画処理
 
